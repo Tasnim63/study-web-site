9422ad219d723af80ab9ee13ec5eab32d8f16a88
Shakiba Alam <shakibaalam092@gmail.com>
2022-07-02 21:49:30 +0600

    theme

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
+import React from 'react';
 
-function Home() {
+const Home = () => {
   return (
     <div>
-      <h1>home</h1>
+      <h1>Hello</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: ["cupcake"],
   },
   plugins: [require("daisyui")],
 }
