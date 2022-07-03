5dc69e57cac19309956cc6a1a8bd24bd723c33a8
Tasnim63 <tamanna202163@gmail.com>
2022-07-03 10:04:52 +0600

    icons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
-<<<<<<< HEAD
-=======
-import Home from './pages/Home/Home';
->>>>>>> tamanna
 
 function App() {
   return (
     <>
     <Navbar></Navbar>
-<<<<<<< HEAD
-=======
-   <Routes>
-
-    <Route path='/' element={<Home></Home>}></Route>
-   </Routes>
->>>>>>> tamanna
     </>
    
   );
